2413d62ed40ae4aa2cc3cf7ff10a67b3dc34eb78
Pankaj Verma <email.pankajverma@gmail.com>
2025-06-24 11:26:53 +0000

    convert /cards/[id] for static export

--- src/app/(app)/cards/[id]/page.tsx
+++ src/app/(app)/cards/[id]/page.tsx
@@ -1,4 +1,17 @@
 
+// The 'generateStaticParams' function is required for Next.js to be able to
+// statically generate all the dynamic routes for this page at build time.
+//
+// NOTE: This function cannot be fully implemented because your card data is
+// stored in Firestore and fetched on the client after a user authenticates.
+// For a true static export, this data would need to be available at build time.
+//
+// By returning an empty array, we are telling Next.js that there are no pages
+// to generate at build time. They will be generated on-demand when first visited.
+export async function generateStaticParams() {
+    return [];
+}
+
 export default function CardDetailRedirectPage() {
   // The redirect logic has been moved to the layout.
   // This page just needs to exist to satisfy the route.
