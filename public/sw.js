0c27084f1396cd7c9f41985fe3e5cdf855621736
Pankaj Verma <email.pankajverma@gmail.com>
2025-06-25 13:07:32 +0000

    i didn't see any change in the menu

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,11 @@
-// A basic service worker for PWA installability
 self.addEventListener('install', (event) => {
-  console.log('Service worker installing...');
-  // You can pre-cache assets here if needed
+  console.log('Service Worker: Installing...');
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service worker activating...');
+  console.log('Service Worker: Activating...');
 });
 
 self.addEventListener('fetch', (event) => {
-  // A simple pass-through fetch handler.
-  // More advanced caching strategies can be implemented here.
   event.respondWith(fetch(event.request));
 });
